e29514f47181f2945e15ea9e354957b44b2180f7
unknown <arthurvonrohrmonteiro@gmail.com>
2024-08-21 17:49:49 -0300

    Linked List adicionado

---
+++ python/pythonZelda.py
@@ -0,0 +1,53 @@
+# kokokokookkokokokokokokko paiton45
+
+class Node:
+    
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+    def __repr__(self):
+        return self.data
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+
+    def add_first(self,node):
+        node.next = self.head
+        self.head = node
+            
+    def add_last(self,data):
+        node = self.head
+        if node == None:
+            self.head = data
+        else:
+            while node.next is not None:
+                node = node.next
+            node.next = data
+
+    def add_node(self,data,position):
+        pointer = 0
+        while pointer != position:
+            node = node.next
+            pointer += 1
+        data.next = node.next
+        node.next = data
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            node.data = str(node.data) 
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+        
+linked_list = LinkedList()
+linked_list.add_last(Node(2))
+linked_list.add_last(Node(3))
+linked_list.add_first(Node(1))
+linked_list.add_node(Node(1.5),2)
+print(linked_list)
