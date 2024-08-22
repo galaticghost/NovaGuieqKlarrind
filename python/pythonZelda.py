434c0ea9447183051fb74d6643492857e2266c83
unknown <arthurvonrohrmonteiro@gmail.com>
2024-08-22 16:49:36 -0300

    Linked List funcional

--- python/pythonZelda.py
+++ python/pythonZelda.py
@@ -17,12 +17,14 @@ class LinkedList:
     def __repr__(self):
         node = self.head
         nodes = []
+        
         while node is not None:
-            node.data = str(node.data) 
             nodes.append(node.data)
             node = node.next
+        
         nodes.append("None")
-        return " -> ".join(nodes)
+        
+        return " -> ".join(str(x)for x in nodes)
 
     def add_first(self,node):
         node.next = self.head
@@ -31,25 +33,30 @@ class LinkedList:
             
     def add_last(self,data):
         node = self.head
+        
         if node == None:
             self.head = data
         else:
             while node.next is not None:
                 node = node.next
             node.next = data
+        
         return None
 
     def add_node(self,data,position):
         if position == 1:
             self.add_first(data)
             return None
+        
         node = self.head
         pointer = 1
+        
         while pointer != position - 1:
             if node.next == None:
                 break
             pointer += 1
             node = node.next
+        
         data.next = node.next
         node.next = data
     
@@ -57,15 +64,23 @@ class LinkedList:
         if position == 1:
             self.head = self.head.next
             return None
+        
         node = self.head
         pointer = 1
+        
         while pointer != position - 1:
             node = node.next
             if node == None:
                 print("No node in this position")
                 return None
             pointer += 1
+        
         node.next = node.next.next
 
-    def search_node(self,node):
+    def search(self,data):
         node = self.head
+        while node.data != data:
+            if node.next == None:
+                return False
+            node = node.next
+        return True
