8218f184d3f22605003a5c32b351078ffb2ebc0d
Arthur Von Rohr Monteiro <arthurvonrohrmonteiro@gmail.com>
2024-10-18 10:25:34 -0300

    Altas modelagens

---
+++ ferias_nestor/dozaluno/modelagemFisica.sql
@@ -0,0 +1,62 @@
+CREATE TABLE pessoa (
+    pk_pessoa INTEGER PRIMARY KEY NOT NULL,
+    nome_pessoa VARCHAR(200) NOT NULL,
+    cpf VARCHAR(20) NOT NULL UNIQUE,
+    cod_matricula VARCHAR(20) NOT NULL UNIQUE,
+    endereco TEXT
+);
+
+CREATE TABLE professor(
+    pk_pessoa INTEGER REFERENCES pessoa NOT NULL,
+    titulacao TEXT NOT NULL,
+    disponibilidade INTEGER NOT NULL
+);
+
+CREATE TABLE curso(
+    pk_curso INTEGER PRIMARY KEY NOT NULL,
+    nome_curso VARCHAR(100) NOT NULL,
+    area VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE turma(
+    pk_turma INTEGER PRIMARY KEY NOT NULL,
+    semestre INTEGER NOT NULL,
+    pk_professor INTEGER REFERENCES pessoa NOT NULL,
+    pk_curso INTEGER REFERENCES curso NOT NULL
+);
+
+CREATE TABLE aluno(
+    pk_pessoa INTEGER REFERENCES pessoa NOT NULL, 
+    pk_curso INTEGER REFERENCES curso NOT NULL,
+    pk_turma INTEGER REFERENCES turma NOT NULL
+);
+
+CREATE TABLE sala(
+    pk_sala INTEGER PRIMARY KEY NOT NULL,
+    nome_sala VARCHAR(100) NOT NULL,
+    capacidade INTEGER NOT NULL
+);
+
+CREATE TABLE disciplina(
+    pk_disciplina INTEGER PRIMARY KEY NOT NULL,
+    nome_disciplina VARCHAR(100) NOT NULL,
+    pk_professor INTEGER REFERENCES pessoa NOT NULL,
+    pk_sala INTEGER REFERENCES sala NOT NULL
+);
+
+CREATE TABLE curso_disciplina(
+    pk_curso_disciplina INTEGER PRIMARY KEY NOT NULL,
+    pk_curso INTEGER REFERENCES curso NOT NULL,
+    pk_disciplina REFERENCES disciplina NOT NULL
+);
+
+CREATE TABLE comodidades(
+    pk_comodidades INTEGER PRIMARY KEY NOT NULL,
+    nome_comodidades VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE comodidades_sala(
+    pk_comodidades_sala INTEGER PRIMARY KEY NOT NULL,
+    pk_comodidades INTEGER REFERENCES comodidades NOT NULL,
+    pk_sala INTEGER REFERENCES sala NOT NULL
+);
