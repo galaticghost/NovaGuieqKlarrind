689d7441431b241aa69571e8d2b952d3599c19a8
Arthur Suse <galaticghost@gmail.com>
2024-10-24 15:04:26 -0300

    Calculadora de idade em js(praque? Não sei)

--- teia/calculadora/script.js
+++ teia/calculadora/script.js
@@ -3,18 +3,31 @@ const dias = document.getElementById("datas");
 const wa = document.getElementById("idade");
 
 function getDatas(){
-    if (dias.value === ''){
+    let aniversario = dias.value
+    if (aniversario === ''){
         alert('Insira um data');
     }
     else{
-        alert(dias.value);
-        wa.innerHTML = dias.value;
-        calculateIdadei();
+        let diasHoje = new Date();
+        let resposta = calculateIdadei(diasHoje,aniversario);
+        wa.innerHTML = "Você tem " + resposta + " anos.";
     }
 }
 
-function calculateIdadei(){
-    
+function calculateIdadei(diasHoje,aniversario){
+    diasHoje = diasHoje.toISOString().split('T')[0]
+    diasHoje = diasHoje.split('-');
+    console.log(diasHoje);
+    aniversario = aniversario.split('-');
+    let idade = diasHoje[0] - aniversario[0];
+    if (diasHoje[1] < aniversario[1]){
+        return idade -1;
+    } else if (diasHoje[1] === aniversario[1]){
+        if (diasHoje[2] < aniversario[2]){
+            return idade - 1;
+        }
+    }
+    return idade;
 }
 
 datas.addEventListener("click",getDatas);
