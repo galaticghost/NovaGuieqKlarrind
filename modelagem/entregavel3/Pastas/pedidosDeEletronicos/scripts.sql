a02464e0b2c5fa8cf94033f43e40b2bf71989776
Arthur Von Rohr Monteiro <arthurvonrohrmonteiro@gmail.com>
2024-11-07 11:16:52 -0300

    7

--- modelagem/entregavel3/Pastas/pedidosDeEletronicos/scripts.sql
+++ modelagem/entregavel3/Pastas/pedidosDeEletronicos/scripts.sql
@@ -1,3 +1,10 @@
+SELECT p.tipo, pp.quantidade AS 'Maior valor', pr.nome  FROM pedido p
+INNER JOIN pedido_produto pp ON pp.pk_pedido = p.pk_pedido
+INNER JOIN produto pr ON pr.pk_produto = pp.pk_pedido
+GROUP BY p.tipo
+ORDER BY MAX(pp.quantidade); -- 1
+
+
 SELECT p.categoria, SUM(pp.valor_total) AS 'Valor total' FROM produto p
 INNER JOIN pedido_produto pp ON pp.pk_produto = p.pk_produto
 GROUP BY categoria
@@ -14,4 +21,7 @@ SELECT c.endereco AS Endereço, c.endereco_2 AS 'Endereço 2', t.telefone AS Tel
 INNER JOIN telefone t ON c.pk_cliente = t.pk_cliente
 GROUP BY c.pk_cliente; -- 7
 
--- 8
+SELECT p.nome, p.preco, p.categoria FROM produto p
+GROUP BY categoria
+HAVING MAX(preco)
+ORDER BY MAX(preco) DESC; -- 8 Seleciona o nome,preco e categoria do produto com maior valor de cada categoria
