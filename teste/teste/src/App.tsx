fd2c55c9dabb17bf24a98b5686d1111b56e698f0
Arthur Suse <galaticghost@gmail.com>
2024-11-11 16:19:28 -0300

    Settando o negócio

--- teste/teste/src/App.tsx
+++ teste/teste/src/App.tsx
@@ -1,5 +1,7 @@
 import Title from "./components/Title.tsx";
 import Button from "./components/Button.tsx";
+import "./app.css";
+
 export default function App() {
   return (
     <div>
--- teste/teste/src/components/Title.tsx
+++ teste/teste/src/components/Title.tsx
@@ -1,3 +1,4 @@
 export default function Title() {
-  return <h1 className="text-xl">O fim</h1>;
+  return <h1 className="text-xl font-serif text-red-600">O fim</h1>;
 }
+
