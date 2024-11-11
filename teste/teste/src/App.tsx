98c07d7c392796824390589913a8194965e37db1
Arthur Von Rohr Monteiro <arthurvonrohrmonteiro@gmail.com>
2024-11-11 11:14:27 -0300

    dia 11 de 11 de 2011?

--- teste/teste/src/App.tsx
+++ teste/teste/src/App.tsx
@@ -1,9 +1,11 @@
 import Title from "./components/Title.tsx";
+import Button from "./components/Button.tsx";
 export default function App() {
   return (
     <div>
       <Title />
-      Hello world
+      <Button />
+      <p>Hello world</p>
     </div>
   );
 }
---
+++ teste/teste/src/components/Button.tsx
@@ -0,0 +1,3 @@
+export default function Button() {
+  return <button>Sou um botao</button>;
+}
