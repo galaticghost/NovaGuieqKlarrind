8061473ca94df5a97eaa2a103e23a18548927a46
Arthur Suse <galaticghost@gmail.com>
2025-01-05 17:49:47 -0300

    put pronto (e exists row concertado)

--- python/apis/aplication.py
+++ python/apis/aplication.py
@@ -36,7 +36,7 @@ def get_bebidas():
 def get_bebida(id):
     sql = 'SELECT nome,descricao FROM bebidas WHERE id = ?;'
     bebidas = database.cursor.execute(sql,(id,)).fetchone()
-    return {"nome":bebidas[0],"descricao":bebidas[1]}
+    return {"nome":bebidas[0],"descricao":bebidas[1]} if bebidas is not None else {"error":"not found"}
 
 @app.route("/bebidas", methods=["POST"])
 def add_bebida():
@@ -45,14 +45,28 @@ def add_bebida():
     database.conexao.commit()
     return {"id":database.cursor.lastrowid}
 
+@app.route("/bebidas/<id>", methods=["PUT"])
+def update_bebida(id):
+    if exists_row(id):
+        return {"error":"not found"}
+
+    sql = 'UPDATE bebidas SET nome = ?, descricao = ? WHERE id = ?'
+    database.cursor.execute(sql,(request.json["nome"],request.json["descricao"],id))
+    database.conexao.commit()
+    return {"message":"200"}
+
 @app.route("/bebidas/<id>", methods=["DELETE"])
 def delete_bebida(id):
-    sql = 'SELECT 1 FROM bebidas WHERE id = ?'
-    exists = database.cursor.execute(sql,(id,))
-    if exists.fetchone() is None:
+    if exists_row(id):
         return {"error":"not found"}
     
     sql = 'DELETE FROM bebidas WHERE id = ?;'
     database.cursor.execute(sql,(id,))
     database.conexao.commit()
     return {"message":"200"}
+
+def exists_row(id):
+    sql = 'SELECT 1 FROM bebidas WHERE id = ?'
+    exists = database.cursor.execute(sql,(id,)).fetchone()
+    if exists is None:
+        return True
