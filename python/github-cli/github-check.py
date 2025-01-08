77be78e67b269df3177f5af7af069ed01793f370
Arthur Suse <galaticghost@gmail.com>
2025-01-08 18:05:32 -0300

    Add types to github checker

--- python/github-cli/github-check.py
+++ python/github-cli/github-check.py
@@ -15,20 +15,46 @@ def get_activity(username: str) -> dict | None:
         if response.status_code == 200:
               return response.json()
         else:
-            print("error")
+            print("ERROR: the user has not been found")
             exit()
 
-def print_activity(activity):
+def print_activity(activity) -> None:
       for action in activity:
             user = action["actor"]["display_login"]
             repo = action["repo"]["name"]
             match action["type"]:
                 case "PushEvent":
-                    print(f"{user} pushed {len(action["payload"]["commits"])} commits to {repo}")
+                    print(f"- {user} pushed {len(action["payload"]["commits"])} commits to {repo}")
                 case "WatchEvent":
-                    print(f"{user} started watching {repo}")
+                    print(f"- {user} started watching {repo}") # Todo
                 case "CreateEvent":
-                    print(f"{user} started {repo}")
+                    print(f"- {user} started {repo}")
+                case "DeleteEvent":
+                    print(f"- {user} has delete {repo}")
+                case "ForkEvent":
+                    print(f"- {user} has forked {repo}")
+                case "GollumEvent":
+                    print(f"- {user}")
+                case "IssueCommentEvent":
+                    print(f"- {user} has comment on {repo}")
+                case "IssuesEvent":
+                    print(f"- {user} has issue on {repo}")
+                case "MemberEvent":
+                    print("-")
+                case "PublicEvent":
+                    pass
+                case "PullRequestEvent":
+                    pass
+                case "PullRequestReviewEvent":
+                    pass
+                case "PullRequestReviewCommentEvent":
+                    pass
+                case "PullRequestReviewThreadEvent":
+                    pass
+                case "ReleaseEvent":
+                    pass
+                case "SponsorshipEvent":
+                    pass
                 case _:
                     print(action)
 
