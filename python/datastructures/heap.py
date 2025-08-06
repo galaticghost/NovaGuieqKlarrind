db5edd771565dfaa6e0cde6292f590af2c1d5c69
Arthur Suse <galaticghost@gmail.com>
2025-08-06 16:06:32 -0300

    Heap finalmente

--- python/datastructures/heap.py
+++ python/datastructures/heap.py
@@ -1,1 +1,43 @@
-import antigravity
+def heapify(arr : list, size: int, i : int):
+    largest = i
+    left_child = i * 2 + 1
+    right_child = i * 2 + 2
+
+    if size > left_child and arr[left_child] > arr[largest]:
+        largest = left_child
+    if size > right_child and arr[right_child] > arr[largest]:
+        largest = right_child
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr,size,largest)
+
+def max_heap(arr):
+    size = len(arr)
+    for i in range(size // 2 - 1 ,-1,-1):
+        heapify(arr,size,i)
+
+def insert(arr,value):
+    arr.append(value)
+    max_heap(arr)
+
+def delete(arr,value):
+    for current_value in arr:
+        if current_value == value:
+            i = arr.index(current_value)
+            break
+    if i == None:
+        return None
+    
+    arr[i], arr[-1] = arr[-1], arr[i]
+    arr.pop()
+    if i < len(arr):
+        heapify(arr,len(arr),i)
+
+
+arr = [0,6,32,2,1,65,7]
+max_heap(arr)
+print(arr)
+print(arr)
+delete(arr,65)
+print(arr)
