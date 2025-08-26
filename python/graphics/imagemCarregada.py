dd7a6cdb33ac89fcab75d5e6eef22432e8f67879
Arthur atitus <arthurvonrohrmonteiro@gmail.com>
2025-08-25 21:58:15 -0300

    Rotate

--- python/graphics/imagemCarregada.py
+++ python/graphics/imagemCarregada.py
@@ -3,6 +3,7 @@ imagem = cv2.cvtColor(cv2.imread("3.jpg"),cv2.COLOR_RGB2GRAY)
 imagem2 = cv2.cvtColor(cv2.imread("3.jpg"),cv2.COLOR_RGB2BGR)
 imagem3 = cv2.cvtColor(cv2.imread("3.jpg"),cv2.COLOR_RGB2HSV)
 imagem4 = cv2.imread("3.jpg",cv2.IMREAD_UNCHANGED)
+rot90 = cv2.rotate(imagem2,cv2.ROTATE_90_CLOCKWISE)
 if imagem is None:
     print("Erro carregando a imagem")
 else:
@@ -10,5 +11,6 @@ else:
     cv2.imshow('2',imagem2)
     cv2.imshow('4',imagem3)
     cv2.imshow('5',imagem4)
+    cv2.imshow('55',rot90)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
