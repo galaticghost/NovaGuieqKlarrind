a3c708a18660f97cafeb9fc5887a17259c433d55
Arthur Suse <galaticghost@gmail.com>
2025-08-28 16:26:00 -0300

    Convolução(roubada) pronta!

--- python/graphics/convolução.py
+++ python/graphics/convolução.py
@@ -1,9 +1,28 @@
 import numpy as np
 from PIL import Image
+from matplotlib import pyplot
 
 IMG_PATH = "atitus1-small.png" #Path da imagem
 img = np.array(Image.open(IMG_PATH).convert("L"))
 
 kernel = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+def convolution(image, kernel):
 
-img2 = np.zeros((img.shape))
+    input_height, input_width = image.shape
+    kernel_height, kernel_width = kernel.shape
+
+    convolved_image = np.zeros((input_height - kernel_height + 1,
+                                 input_width - kernel_width + 1))
+    
+    for i in range(0,len(convolved_image) ):
+        for j in range(0, len(convolved_image[0])):
+            for ii in range(0, kernel_height):
+                for jj in range(0, kernel_width):
+                  convolved_image[i,j] += image[i+ii, j+jj] * kernel[ii,jj]
+    
+    return convolved_image
+
+img = convolution(img, kernel)
+
+pyplot.imshow(img,cmap='gray')
+pyplot.show()
--- python/graphics/test.py
+++ python/graphics/test.py
@@ -5,8 +5,8 @@ from PIL import Image
 img2 = np.array(Image.open("3.jpg"))
 #img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
 img = np.ones((800,600,3),dtype=np.uint8) * 255
-cv2.line(img2,(0,0),(550,750),(255,0,0),5)
-cv2.imshow('Bola2',img)
+#cv2.line(img2,(0,0),(550,750),(255,0,0),5)
+#cv2.imshow('Bola2',img)
 cv2.imshow("vamp",img2)
 
 
