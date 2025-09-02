621b89747cd719c9f93bb479203b32303d6404b5
Arthur Suse <galaticghost@gmail.com>
2025-09-02 10:05:17 -0300

    pequenas mudanças

--- python/graphics/canalVazio.py
+++ python/graphics/canalVazio.py
@@ -3,9 +3,23 @@ from PIL import Image
 from matplotlib import pyplot
 IMG_PATH = "RGB-small.png"
 img = np.array(Image.open(IMG_PATH).convert("RGB"))
-matriz_zero = np.zeros((img.shape),dtype=img.dtype)
-matriz_zero[:,:,0] = img[:,:,0] # mudar o ultimo index para mudar a cor
+matriz_vermelha = np.zeros((img.shape),dtype=img.dtype)
+matriz_vermelha[:,:,0] = img[:,:,0]
 
-pyplot.imshow(matriz_zero)
+matriz_verde = np.zeros((img.shape),dtype=img.dtype)
+matriz_verde[:,:,1] = img[:,:,1] 
+
+matriz_azul = np.zeros((img.shape),dtype=img.dtype)
+matriz_azul[:,:,2] = img[:,:,2]
+
+pyplot.imshow(matriz_vermelha)
 pyplot.title('Canal Vermelho')
+pyplot.show()
+
+pyplot.imshow(matriz_verde)
+pyplot.title('Canal Verde')
+pyplot.show()
+
+pyplot.imshow(matriz_azul)
+pyplot.title('Canal Azul')
 pyplot.show()
