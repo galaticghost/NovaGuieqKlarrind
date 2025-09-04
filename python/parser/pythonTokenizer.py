55bb9057e668a674d3f07b8ec274358aec4727a5
Arthur atitus <arthurvonrohrmonteiro@gmail.com>
2025-09-04 20:47:25 -0300

    Não tá pronto

--- python/parser/pythonTokenizer.py
+++ python/parser/pythonTokenizer.py
@@ -16,9 +16,7 @@ with open("texto.txt","r") as f:
         tokens.append(('TAG_FECHA',match.group(0)))
         print(match.group(0))
 
-    for match in re.finditer(r">([^<>]+)(?=<|$)",y):
-        if match.group(0) == ">\n    " or ">\n  " == match.group(0):
-            break
-        tokens.append(('TAG_TEXTO',match.group(0)))
-
+    for match in re.finditer(r">\s*([^<>]+)(?=<|$)",y):
+        tokens.append(('TAG_TEXTO',match.group()))
+    
 print(tokens)
