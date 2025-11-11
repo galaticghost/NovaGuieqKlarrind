a0ac100b0d62aaa6661444abcaf653c451cef877
Arthur Suse <galaticghost@gmail.com>
2025-11-11 13:20:58 -0300

    Textura modificada

--- python/graphics/3d/trabalho/main.py
+++ python/graphics/3d/trabalho/main.py
@@ -1,25 +1,30 @@
-import glfw
 from OpenGL.GL import *
+from fps import FPSCounter
+from utils import *
+import glfw
 import numpy as np
 import math
-from fps import FPSCounter
 import cv2
 
 def main():
     global camera_pos, camera_front, camera_up, window, first_mouse,yaw,pitch,last_x,last_y,rot_x,rot_y
+
     # 1. Initialize GLFW
     if not glfw.init():
         print("Erro: Não foi possível inicializar o GLFW.")
         return
-    rot_x = rot_y = 0.0
+    
     # Request OpenGL Core Profile 3.3
     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
     # glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, glfw.TRUE)  # Uncomment if needed (MacOS)
 
+    win_height = 2160
+    win_width = 3840
+
     # 2. Criação da Janela de Aplicação
-    window = glfw.create_window(800, 600, "Quadrado", None, None)
+    window = glfw.create_window(win_width, win_height, "Quadrado", None, None)
     if not window:
         glfw.terminate()
         print("Erro: Não foi possível criar a janela GLFW.")
@@ -30,11 +35,8 @@ def main():
     glfw.set_input_mode(window,glfw.CURSOR,glfw.CURSOR_DISABLED)
     glfw.set_cursor_pos_callback(window, mouse_callback)
 
-    texture = cv2.imread('texture.webp',cv2.IMREAD_COLOR)
-    texture = cv2.cvtColor(texture, cv2.COLOR_BGR2RGB)
-    texture = cv2.flip(texture, 0)
-
-    quadrado = np.array([0.3, 0.3, -1.0, 1.0, 1.0, # T1 V1
+    # Quadrado
+    quadrado_vertex = np.array([0.3, 0.3, -1.0, 1.0, 1.0, # T1 V1
                         0.3, -0.3, -1.0, 1.0, 0.0, # T1 V2
                         -0.3, -0.3, -1.0, 0.0, 0.0, # T1 V3
                         -0.3, -0.3, -1.0, 0.0, 0.0, # T2 V1
@@ -42,35 +44,43 @@ def main():
                         0.3, 0.3, -1.0, 1.0, 1.0, # T2 V3
     ],dtype=np.float32)
 
-    vao_qua, vbo_qua = setup_geometry(quadrado)
+    vao_qua, vbo_qua = setup_geometry_uv(quadrado_vertex)
+
+    # Textura
+    texture = cv2.imread('texture.jpg',cv2.IMREAD_COLOR)
+    texture = cv2.cvtColor(texture, cv2.COLOR_BGR2RGB)
+    texture = cv2.flip(texture, 0) # Inverte verticalmente
+    tex_height, tex_width , _ = texture.shape
 
     texture_id = glGenTextures(1)
     glBindTexture(GL_TEXTURE_2D,texture_id)
-
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,640,587,0,GL_RGB, GL_UNSIGNED_BYTE,texture)
-
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,tex_width,tex_height,0,GL_RGB, GL_UNSIGNED_BYTE,texture)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    
 
-    camera_pos = np.array([0.0, 0.0,3.0], dtype=np.float32) #world pos
-    camera_front = np.array([0.0, 0.0, -1.0], dtype=np.float32) #facing
-    camera_up = np.array([0.0, 1.0, 0.0], dtype=np.float32) #up
-
-    scale_m = create_scale(1.0, 1.0, 1.0)
+    # Matrizes do model
+    scale_m = create_scale(2.0, 1.5, 1.0)
     rotation_x = create_rotation(0.0,"x")
     rotation_y = create_rotation(0.0,"y")
     rotation_z = create_rotation(0.0,"z")
     rotation_m = rotation_x @ rotation_y @ rotation_z
-    translation_m = create_translation(0.0, 0.0, 0.0)
+    translation_m = create_translation(0.0, 0.0, 1.0)
     model = translation_m @ rotation_m @ scale_m
+
+
+    # Matriz da câmera
+    camera_pos = np.array([0.0, 0.0,3.0], dtype=np.float32) #world pos
+    camera_front = np.array([0.0, 0.0, -1.0], dtype=np.float32) #facing
+    camera_up = np.array([0.0, 1.0, 0.0], dtype=np.float32) #up
     view = create_view(camera_pos,camera_front,camera_up)
-    projection = create_projection(45.0,800/600,0.1,100.0)
+
+    # Matriz de projeção
+    projection = create_projection(45.0,win_width/win_height,0.1,100.0)
 
     mvpshader = create_shader(GL_VERTEX_SHADER,"shadermvp.vert")
-    fragshader = create_shader(GL_FRAGMENT_SHADER,"shaderfrag.frag")
+    fragshader = create_shader(GL_FRAGMENT_SHADER,"convolution.frag")
 
     shader_program = glCreateProgram()
     glAttachShader(shader_program,mvpshader)
@@ -85,22 +95,32 @@ def main():
     glUniformMatrix4fv(view_loc, 1, GL_FALSE, view.flatten())
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection.flatten())
 
+    # variáveis para a convolução
+    kernel = np.array([[[1/16,1/8,1/16],[1/8,1/4,1/8],[1/16,1/8,1/16]]],
+                      dtype=np.float32)
+
+    kernel_loc = glGetUniformLocation(shader_program,"kernel")
+    size_loc = glGetUniformLocation(shader_program,"texSize")
     texture_loc = glGetUniformLocation(shader_program, "frameColor")
     glUniform1i(texture_loc, 0)
+    glUniform2fv(size_loc,1,np.array([1.0/tex_width,1.0/tex_height],dtype=np.float32))
+    glUniformMatrix3fv(kernel_loc,1,GL_FALSE,kernel)
 
     #Espeficamos as operações de viewport
-    glViewport(0, 0, 800, 600)
+    glViewport(0, 0, win_width, win_height)
 
+    # Globais para a camera com o mouse
     first_mouse = True
     yaw = -90.0
     pitch = 0
     last_y = 300
     last_x = 400
+    rot_x = rot_y = 0.0
 
+    # Comento depois TODO
     glEnable(GL_DEPTH_TEST)
 
     fps_counter = FPSCounter(average_over=30, stats_interval=5.0)
-    win_width, win_height = glfw.get_window_size(window)
     fps_counter.initialize_text_rendering(win_width, win_height)
     fps_counter.enable_stats_printing(True)
 
@@ -152,98 +172,6 @@ def main():
     glDeleteProgram(shader_program)
     glDeleteShader(mvpshader)
     glDeleteShader(fragshader)
-
-def setup_geometry(vertex):
-    vao = glGenVertexArrays(1)
-    vbo = glGenBuffers(1)
-    glBindVertexArray(vao)
-    glBindBuffer(GL_ARRAY_BUFFER, vbo)
-    glBufferData(GL_ARRAY_BUFFER, vertex.nbytes, vertex, GL_STATIC_DRAW)
-
-    stride = 5 * vertex.itemsize
-
-    # Atributo 0: Posição (3 floats)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(0))
-    glEnableVertexAttribArray(0)
-
-    # Atributo 1: UV (2 floats)
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(3 * vertex.itemsize))
-    glEnableVertexAttribArray(1)
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
-    glBindVertexArray(0)
-
-    return vao,vbo
-
-def create_shader(shader_type,path):
-    shader = glCreateShader(shader_type)
-    with open(path,'r') as file:
-        glShaderSource(shader,file.read())
-    glCompileShader(shader)
-    if not glGetShaderiv(shader, GL_COMPILE_STATUS):
-        print(glGetShaderInfoLog(shader).decode())
-    return shader
-
-def create_view(camera_pos,camera_front,camera_up):
-    z_axis = -camera_front / np.linalg.norm(camera_front)
-    x_axis = np.cross(camera_up, z_axis)
-    x_axis = x_axis / np.linalg.norm(x_axis)
-    y_axis = np.cross(z_axis, x_axis)
-
-    view = np.array([
-    [x_axis[0], y_axis[0], z_axis[0], 0], #s
-    [x_axis[1], y_axis[1], z_axis[1], 0], #u
-    [x_axis[2], y_axis[2], z_axis[2], 0], #f
-    [-np.dot(x_axis, camera_pos), -np.dot(y_axis, camera_pos), -np.dot(z_axis, camera_pos), 1]
-    ], dtype=np.float32)
-    return view
-
-def create_projection(fov,aspect,near,far):
-    f = 1.0 / math.tan(math.radians(fov) / 2.0)
-    proj = np.zeros((4, 4), dtype=np.float32)
-
-    proj[0, 0] = f / aspect
-    proj[1, 1] = f
-    proj[2, 2] = (far + near) / (near - far)
-    proj[2, 3] = -1.0
-    proj[3, 2] = (2.0 * far * near) / (near - far)
-    return proj
-
-def create_scale(fx,fy,fz):
-    scale = np.array([[fx,0,0,0],[0,fy,0,0],[0,0,fz,0],[0,0,0,1]],dtype=np.float32)
-    return scale
-
-def create_rotation(angle,axis):
-    c = math.cos(angle)
-    s = math.sin(angle)
-
-    if axis == 'x':
-        return np.array([
-            [1, 0, 0, 0],
-            [0, c, -s, 0],
-            [0, s, c, 0],
-            [0, 0, 0, 1]
-        ], dtype=np.float32)
-
-    elif axis == 'y':
-        return np.array([
-            [c, 0, s, 0],
-            [0, 1, 0, 0],
-            [-s, 0, c, 0],
-            [0, 0, 0, 1]
-        ], dtype=np.float32)
-
-    elif axis == 'z':
-        return np.array([
-            [c, -s, 0, 0],
-            [s, c, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ], dtype=np.float32)
-
-def create_translation(x,y,z):
-    translation = np.array([[1,0,0,x],[0,1,0,y],[0,0,1,z],[0,0,0,1]],dtype=np.float32)
-    return translation
 
 def keys():
     global camera_pos,camera_front,camera_up,window,rot_x,rot_y
